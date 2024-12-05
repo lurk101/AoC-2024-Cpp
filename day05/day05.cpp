0b5de4b6a6dc6ef9def63ca30ec4611d6ddfb5be
lurk101 <lurkone0one@gmail.com>
2024-12-05 12:39:41 -0500

    day 5

---
+++ day05/day05.cpp
@@ -0,0 +1,40 @@
+#include <algorithm>
+#include <chrono>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+using namespace chrono;
+
+static unordered_map<int, unordered_set<int>> numbers;
+static int part1, part2;
+
+static bool LT(int lhs, int rhs) { return numbers[lhs].find(rhs) != numbers[lhs].end(); }
+
+int main() {
+    auto start = high_resolution_clock::now();
+    ifstream fi("day05.txt");
+    string line;
+    while (getline(fi, line) && (line != ""))
+        numbers[stoi(line.substr(0, 2))].insert(stoi(line.substr(3, 2)));
+    while (getline(fi, line)) {
+        vector<int> ordered;
+        for (int i = 0; i < line.length(); i += 3) ordered.push_back(stoi(line.substr(i, 2)));
+        auto unOrdered = ordered;
+        sort(ordered.begin(), ordered.end(), LT);
+        if (unOrdered == ordered)
+            part1 += ordered[ordered.size() / 2];
+        else
+            part2 += ordered[ordered.size() / 2];
+    }
+    cout << "Day 5: Print Queue" << endl
+         << "part 1   - " << part1 << endl
+         << "part 2   - " << part2 << endl
+         << "run time - "
+         << duration_cast<microseconds>(high_resolution_clock::now() - start).count() / 1000.0
+         << " ms." << endl;
+}
