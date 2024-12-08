f14966c2ea3f7c43f40a9497dd3946f655c7a409
lurk101 <lurkone0one@gmail.com>
2024-12-07 22:18:24 -0500

    update day07.cpp

--- day07/day07.cpp
+++ day07/day07.cpp
@@ -1,113 +1,87 @@
 #include <chrono>
-#include <cmath>
 #include <fstream>
 #include <iostream>
-#include <string>
+#include <string_view>
 #include <vector>
 
 using namespace std;
 using namespace chrono;
 
-static vector<string> split(string s, char c) {
-    vector<string> tokens;
-    size_t pos = 0;
-    string token;
-    while ((pos = s.find(c)) != string::npos) {
-        token = s.substr(0, pos);
-        tokens.push_back(token);
-        s.erase(0, pos + 1);
+static auto getNumbers(string_view const line, char delim) {
+    vector<size_t> parts;
+    size_t strt{};
+    auto const len = line.size();
+    while (strt < len) {
+        size_t end = line.find(delim, strt);
+        if (end == string_view::npos) end = len;
+        size_t num{};
+        from_chars(line.data() + strt, line.data() + end, num);
+        parts.push_back(num);
+        strt = end + 1;
     }
-    tokens.push_back(s);
-    return tokens;
+    return parts;
 }
-bool match1(const int64_t ans, const vector<int>& oprnds) {
-    if (oprnds.size() == 1) return oprnds[0] == ans;
-    for (int64_t i = 0; i < pow(2, oprnds.size() - 1) + 1; ++i) {
-        int64_t res = oprnds[0];
-        for (int j = 1; j < oprnds.size(); ++j) {
-            bool is_plus = ((1 << (j - 1)) & i) == 0;
-            const int o = oprnds[j];
-            res = is_plus ? res + o : res * o;
-            if (res == ans) return true;
+
+static constexpr bool divMod(size_t dividend, size_t divisor, size_t& quotient, size_t& remainder) {
+    if (divisor == 0) return false;
+    quotient = dividend / divisor;
+    remainder = dividend % divisor;
+    return true;
+}
+
+static bool valid(size_t i, size_t testVal, vector<size_t> const& eq, bool concat) {
+    auto const curr = eq[i];
+    if (i == 0) return curr == testVal;
+    size_t divVal, modVal;
+    if (divMod(testVal, curr, divVal, modVal) && modVal == 0 && valid(i - 1, divVal, eq, concat))
+        return true;
+    if (testVal > curr && valid(i - 1, testVal - curr, eq, concat)) return true;
+    if (concat) {
+        size_t multiplier{1}, temp{curr};
+        while (temp > 0) {
+            multiplier *= 10;
+            temp /= 10;
+        }
+        if (divMod(testVal, multiplier, divVal, modVal) && modVal == curr &&
+            valid(i - 1, divVal, eq, concat)) {
+            return true;
         }
     }
     return false;
 }
 
-int64_t concat(int64_t a, int64_t b) {
-    int64_t magnitude = 1;
-    while (magnitude <= b) magnitude *= 10;
-    return magnitude * a + b;
-}
-
-bool match2(const int64_t ans, const vector<int>& oprnds) {
-    if (oprnds.size() == 1) return oprnds[0] == ans;
-    const int exp = 2 * int(oprnds.size()) - 2;
-    for (int64_t i = 0; i <= pow(2, exp); ++i) {
-        int64_t res = oprnds[0];
-        for (int j = 1; j < oprnds.size(); ++j) {
-            const int o = oprnds[j];
-            const int indices = 2 * j - 2;
-            const int want = (3 << indices) & i;
-            const int adjusted = want >> indices;
-            switch (adjusted) {
-            case 0:
-                res = res + o;
-                break;
-            case 1:
-                res = res * o;
-                break;
-            case 2:
-            case 3:
-                res = concat(res, o);
-                break;
-            }
-            if (res > ans) break;
-        }
-        if (res == ans) return true;
+static pair<size_t, size_t> solve(vector<string_view> const& in) {
+    size_t part1(0);
+    size_t part2(0);
+    for (auto const line : in) {
+        auto const colon{line.find(':')};
+        auto const eq_view{line.substr(colon + 2)};
+        auto const eq{getNumbers(eq_view, ' ')};
+        auto const eq_len{eq.size()};
+        size_t val = 0;
+        from_chars(line.data(), line.data() + colon, val);
+        if (valid(eq_len - 1, val, eq, false)) part1 += val;
+        if (valid(eq_len - 1, val, eq, true)) part2 += val;
     }
-    return false;
+    return {part1, part2};
 }
 
 int main() {
     auto start = high_resolution_clock::now();
     ifstream fi("day07.txt");
-    struct fmt {
-        fmt(int64_t a, vector<int> n) : ans(a), nums(n) {}
-        int64_t ans;
-        vector<int> nums;
-    };
-    vector<fmt> inputs;
-    while (true) {
-        string line;
-        getline(fi, line);
-        if (line.empty())
-            break;
-        vector<string> sp = split(line, ':');
-        const int64_t line_ans = stoll(sp[0]);
-        vector<string> sp2 = split(sp[1], ' ');
-        vector<int> numbers;
-        numbers.reserve(sp2.size());
-        for (const string& s : sp2) {
-            if (s == "")
-                continue;
-            numbers.push_back(stoi(s));
+    string buffer;
+    buffer.reserve(1024 * 1024);
+    buffer.assign(istreambuf_iterator<char>(fi), istreambuf_iterator<char>());
+    vector<string_view> in;
+    size_t strt = 0;
+    for (size_t i = 0; i < buffer.size(); ++i) {
+        if (buffer[i] == '\n' || buffer[i] == '\r') {
+            in.emplace_back(buffer.data() + strt, i - strt);
+            strt = i + 1;
         }
-        inputs.push_back(fmt(line_ans, numbers));
     }
-    int64_t p1 = 0;
-    for (const fmt& format : inputs) {
-        const int64_t ans = format.ans;
-        const vector<int> oprnds = format.nums;
-        if (match1(ans, oprnds))
-            p1 += ans;
-    }
-    int64_t p2 = 0;
-    for (const fmt& f : inputs) {
-        const int64_t ans = f.ans;
-        const vector<int> oprnds = f.nums;
-        if (match1(ans, oprnds) || match2(ans, oprnds)) p2 += ans;
-    }
+    if (strt < buffer.size()) in.emplace_back(buffer.data() + strt, buffer.size() - strt);
+    auto const [p1, p2]{solve(in)};
     cout << "Day 7: Bridge Repair" << endl
          << "part 1   - " << p1 << endl
          << "part 2   - " << p2 << endl
