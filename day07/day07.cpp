5459056ef73f2f92be4704938e1dc1a73caee7e4
lurk101 <lurkone0one@gmail.com>
2024-12-19 14:32:55 -0500

    update day7

--- day07/day07.cpp
+++ day07/day07.cpp
@@ -7,10 +7,10 @@
 using namespace std;
 using namespace chrono;
 
-static vector<size_t> getNumbers(const string_view& s, char delim) {
-    size_t pos = 0, posEnd;
+static vector<uint64_t> getNumbers(const string_view& s, char delim) {
+    uint64_t pos = 0, posEnd;
     string token;
-    vector<size_t> r;
+    vector<uint64_t> r;
     while ((posEnd = s.find(delim, pos)) != string_view::npos) {
         token = s.substr(pos, posEnd - pos);
         pos = posEnd + 1;
@@ -21,22 +21,23 @@ static vector<size_t> getNumbers(const string_view& s, char delim) {
     return r;
 }
 
-static constexpr bool divMod(size_t dividend, size_t divisor, size_t& quotient, size_t& remainder) {
+static constexpr bool divMod(uint64_t dividend, uint64_t divisor, uint64_t& quotient,
+                             uint64_t& remainder) {
     if (divisor == 0) return false;
     quotient = dividend / divisor;
     remainder = dividend % divisor;
     return true;
 }
 
-static bool valid(size_t i, size_t testVal, vector<size_t> const& eq, bool concat) {
+static bool valid(uint64_t i, uint64_t testVal, vector<uint64_t> const& eq, bool concat) {
     auto const curr = eq[i];
     if (i == 0) return curr == testVal;
-    size_t divVal, modVal;
+    uint64_t divVal, modVal;
     if (divMod(testVal, curr, divVal, modVal) && modVal == 0 && valid(i - 1, divVal, eq, concat))
         return true;
     if (testVal > curr && valid(i - 1, testVal - curr, eq, concat)) return true;
     if (concat) {
-        size_t multiplier{1}, temp{curr};
+        uint64_t multiplier{1}, temp{curr};
         while (temp > 0) {
             multiplier *= 10;
             temp /= 10;
@@ -49,15 +50,15 @@ static bool valid(size_t i, size_t testVal, vector<size_t> const& eq, bool conca
     return false;
 }
 
-static pair<size_t, size_t> solve(vector<string_view> const& in) {
-    size_t part1(0);
-    size_t part2(0);
+static pair<uint64_t, uint64_t> solve(vector<string_view> const& in) {
+    uint64_t part1(0);
+    uint64_t part2(0);
     for (auto const line : in) {
         auto const colon{line.find(':')};
         auto const eq_view{line.substr(colon + 2)};
         auto const eq{getNumbers(eq_view, ' ')};
         auto const eq_len{eq.size()};
-        size_t val = stoll(string(line));
+        uint64_t val = stoll(string(line));
         if (valid(eq_len - 1, val, eq, false)) part1 += val;
         if (valid(eq_len - 1, val, eq, true)) part2 += val;
     }
@@ -71,8 +72,8 @@ int main() {
     buffer.reserve(1024 * 1024);
     buffer.assign(istreambuf_iterator<char>(fi), istreambuf_iterator<char>());
     vector<string_view> in;
-    size_t strt = 0;
-    for (size_t i = 0; i < buffer.size(); ++i) {
+    uint64_t strt = 0;
+    for (uint64_t i = 0; i < buffer.size(); ++i) {
         if (buffer[i] == '\n' || buffer[i] == '\r') {
             in.emplace_back(buffer.data() + strt, i - strt);
             strt = i + 1;
