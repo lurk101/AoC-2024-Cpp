a70b00cf31df41f782e7e4b2927f2cba5815d76c
lurk101 <lurkone0one@gmail.com>
2024-12-19 10:22:56 -0500

    day19

---
+++ day19/day19.cpp
@@ -0,0 +1,61 @@
+#include <chrono>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+using namespace chrono;
+
+static vector<string> patterns;
+static vector<string> designs;
+
+static vector<string> Split(const string& s, const string delim) {
+    size_t pos = 0, posEnd;
+    string token;
+    vector<string> r;
+    while ((posEnd = s.find(delim, pos)) != string::npos) {
+        token = s.substr(pos, posEnd - pos);
+        pos = posEnd + delim.size();
+        r.push_back(token);
+    }
+    r.push_back(s.substr(pos));
+    return r;
+}
+
+static uint64_t Ways(const string& design, int partNum) {
+    int n = design.size();
+    vector<uint64_t> dp(n + 1, 0);
+    dp[0] = 1;
+    for (int i = 1; i <= n; i++)
+        for (string p : patterns) {
+            int len = p.size();
+            if (partNum == 1) {
+                if (i >= len && design.substr(i - len, len) == p) dp[i] = dp[i] || dp[i - len];
+            } else if (i >= len && design.substr(i - len, len) == p)
+                dp[i] += dp[i - len];
+        }
+    return dp[n];
+}
+
+static uint64_t Part(int partNum) {
+    uint64_t s = 0;
+    for (string d : designs) s += Ways(d, partNum);
+    return s;
+}
+
+int main() {
+    auto strt = high_resolution_clock::now();
+    ifstream fi("day19.txt");
+    string line;
+    getline(fi, line);
+    patterns = Split(line, ", ");
+    getline(fi, line);
+    while (getline(fi, line)) designs.push_back(line);
+    cout << "Day 19: Linen Layout" << endl
+         << "Part 1   - " << Part(1) << endl
+         << "Part 2   - " << Part(2) << endl
+         << "Run time - "
+         << duration_cast<microseconds>(high_resolution_clock::now() - strt).count() / 1e3 << " ms."
+         << endl;
+}
