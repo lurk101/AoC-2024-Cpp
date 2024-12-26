389581f1dcd52db0535fc35416675625a2f02906
lurk101 <lurkone0one@gmail.com>
2024-12-26 09:07:45 -0500

    update day25

--- day25/day25.cpp
+++ day25/day25.cpp
@@ -9,46 +9,39 @@
 using namespace std;
 using namespace chrono;
 
-static vector<set<pair<int8_t, int8_t>>> keys, locks;
+static vector<uint32_t> keys, locks;
 
 int main() {
     auto strt = high_resolution_clock::now();
     ifstream fi("day25.txt");
     string line;
     int y = 0;
-    set<pair<int8_t, int8_t>> keyOrLock;
+    uint32_t keyOrLock = 0;
     while (getline(fi, line)) {
         if (line.empty()) {
-            if (keyOrLock.count({0, 0}))
-                locks.push_back(keyOrLock);
-            else
-                keys.push_back(keyOrLock);
             y = 0;
-            keyOrLock.clear();
+            keyOrLock = 0;
             continue;
         }
-        for (int x = 0; x < line.size(); x++)
-            if (line[x] == '#') keyOrLock.insert({x, y});
+        if ((y > 0) && (y < 6)) {
+            for (int x = 0; x < line.size(); x++) {
+                keyOrLock <<= 1;
+                if (line[x] == '#') keyOrLock |= 1;
+            }
+        } else {
+            if (y == 6) {
+                if (line[0] == '#')
+                    locks.push_back(keyOrLock);
+                else
+                    keys.push_back(keyOrLock);
+            }
+        }
         y++;
     }
-    if (keyOrLock.count({0, 0}))
-        locks.push_back(keyOrLock);
-    else
-        keys.push_back(keyOrLock);
     int sum = 0;
-    for (const auto& l : locks)
-        for (const auto& k : keys) {
-            map<pair<int8_t, int8_t>, int8_t> merged;
-            for (const auto& pos : k) merged[pos]++;
-            for (const auto& pos : l) merged[pos]++;
-            bool match = true;
-            for (const auto& m : merged)
-                if (m.second != 1) {
-                    match = false;
-                    break;
-                }
-            if (match) sum++;
-        }
+    for (const auto& lock : locks)
+        for (const auto& key : keys)
+            if ((lock & key) == 0) sum++;
     cout << "Day 25: Code Chronicle" << endl
          << "Part 1   - " << sum << endl
          << "Part 2   - free star" << endl
